8c097ba7993cf5f32c84cff034a1f7a78eebad00
Anatoliy Naboychtenko <skuminok@gmail.com>
2024-08-31 22:05:15 +0500

    added_description

--- testC#/obj/Debug/net8.0/testC#.AssemblyInfo.cs
+++ testC#/obj/Debug/net8.0/testC#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("testC#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98210a7690c54c42494d14eb66ee5b8d9c3500bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf65302dbe5000348d71be36149464c735402e08")]
 [assembly: System.Reflection.AssemblyProductAttribute("testC#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("testC#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
